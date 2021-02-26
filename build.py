f66ca0a324a2841e0a796e2538f17228ac44b100
Arjun <44180430+ArjunSahlot@users.noreply.github.com>
2021-02-25 18:50:59 -0800

    Update build.py

--- build.py
+++ build.py
@@ -17,28 +17,8 @@ print("Unzipping")
 with zipfile.ZipFile(tmp, 'r') as zip:
     zip.extractall(dir)
 
-<<<<<<< HEAD
 print("Cleaning up")
 final = os.path.join(dir, file.split("-")[0]
 os.rename(os.path.join(dir, file), final))
-=======
-os.rename(os.path.join(dir, file), os.path.join(dir, file.split("-")[0]))
->>>>>>> df548bb3cff6c79c521eecf010146838104f22af
-os.remove(os.path.join(tmp))
-
-with open(os.path.join(final, "requirements.txt"), "r") as f:
-    packages = f.read().split("
-")
-
-if sys.platform == "windows":
-    cmd = "pip install "
-else:
-    cmd = "pip3 install "
-
-print("Installing packages")
-for package in packages:
-    if "n" in input(f"Install" + package + "? [y/n] ").lower():
-        continue
-    os.system(cmd.format(package))
 
 print("Done!")
